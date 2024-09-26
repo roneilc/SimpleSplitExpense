e5b00ad11697d7cd1f25ff20fa0560ca01411acc
roneilc <rcabalatungan@ups.com>
2024-09-26 18:05:54 -0400

    Initial Commit

---
+++ split-expense-calculator/src/main/java/com/splitexpense/split_expense_calculator/SplitExpenseCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.splitexpense.split_expense_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitExpenseCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitExpenseCalculatorApplication.class, args);
+	}
+
+}
---
+++ split-expense-calculator/src/test/java/com/splitexpense/split_expense_calculator/SplitExpenseCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitexpense.split_expense_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitExpenseCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
