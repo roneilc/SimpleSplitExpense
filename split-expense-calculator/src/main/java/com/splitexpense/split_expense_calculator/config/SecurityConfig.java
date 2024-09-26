e36df4fec2ffd61cf258b53f1af2544b14c049f4
roneilc <rcabalatungan@ups.com>
2024-09-26 18:33:37 -0400

    add security config and expense controller

---
+++ split-expense-calculator/src/main/java/com/splitexpense/split_expense_calculator/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.splitexpense.split_expense_calculator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/auth/**").permitAll()  
+            .anyRequest().authenticated()        // All other routes require authentication
+            .and()
+            .httpBasic();  
+        return http.build();
+    }
+}
